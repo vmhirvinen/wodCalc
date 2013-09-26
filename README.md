7f7d46c820a438eb40a47c56216e8002f27ff35a
VM Hirvinen <vm_hirvinen@hotmail.com>
2013-09-26 22:33:55 +0300

    added buttons to enter weight

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 wodCalc
 =======
 
-simple workout calculator optimized for mobile usage
+simple workout calculator optimized for mobile usage using AngularJS
+
+Wendler 5/3/1
+-counts work set weights for given wave based on maximum weight
+-counts estimated maximum based on last set reps
+-Check http://www.jimwendler.com/ for more info
--- app/js/app.js
+++ app/js/app.js
@@ -5,27 +5,41 @@ angular.module('wendlerCalculator', ['ui.bootstrap']);
 var WendlerCtrl;
 
 WendlerCtrl = function($scope, $http) {
-  $scope.maxweight = 100;
   $scope.waves = [
     {name: "Wave 1", value: 0 },
     {name: "Wave 2", value: 1 },
     {name: "Wave 3", value: 2 },
     {name: "Wave 4", value: 3 }
-  ];
-  $scope.wave = $scope.waves[0];
+  ];  
+
+  $scope.maxweight = 100;
+  $scope.hundreds = { value: 0 };
+  $scope.tens = { value: 0 };
+  $scope.singles = { value: 0 };
+  $scope.wave = {value: $scope.waves[0] };
+  $scope.hundredValues = [0, 100, 200, 300];
+  $scope.tenValues = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
+  $scope.singleValues = [0, 2.5, 5, 7.5];
 
   $scope.sets = [{reps: 0, weight: 0}, {reps: 0, weight: 0}, {reps: 0, weight: 0}];
 
+  $scope.$watch('hundreds.value + tens.value + singles.value', function(newValue, oldValue) {
+    $scope.countMax();
+  });
+
   $scope.countWeights = function() {
+    var curWave = $scope.wave.value.value;
     var max = $scope.maxweight?$scope.maxweight.toString().replace(/,/, "."):0;    
     var sets = [[5, 5, "5+"], [3, 3, "3+"], [5, 3, "1+"], [5, 5, 5]];
     var pct = [[0.65, 0.75, 0.85], [0.70, 0.80, 0.90], [0.75, 0.85, 0.95], [0.45, 0.55, 0.65]];
     var i = 0;
-    for (i = 0; i < pct[$scope.wave.value].length; i++) {
-      $scope.sets[i].weight = Math.ceil((pct[$scope.wave.value][i] * (max * 0.9)) / 2.5) * 2.5;
-      $scope.sets[i].reps = sets[$scope.wave.value][i];
-    }
-
-    $scope.counted_max = "";    
+    for (i = 0; i < pct[curWave].length; i++) {
+      $scope.sets[i].weight = Math.ceil((pct[curWave][i] * (max * 0.9)) / 2.5) * 2.5;
+      $scope.sets[i].reps = sets[curWave][i];
+    }    
   };  
+
+  $scope.countMax = function() {
+    $scope.maxweight = $scope.hundreds.value + $scope.tens.value + $scope.singles.value;
+  };
 };
